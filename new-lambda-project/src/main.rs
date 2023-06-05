ff672fc96360fa3df9382f2627855f7bf675c800
Sumeet Naik <25149022+sumeetweb@users.noreply.github.com>
2023-06-05 04:09:31 +0000

    jwt verify

---
+++ new-lambda-project/src/main.rs
@@ -0,0 +1,45 @@
+
+use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+use jsonwebtokens_cognito::KeySet;
+
+pub type LambdaError = Box<dyn std::error::Error + Send + Sync + 'static>;
+/// This is the main body for the function.
+/// Write your code inside it.
+/// There are some code example in the following URLs:
+/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
+async fn function_handler(event: Request) -> Result<Response<Body>, LambdaError> {
+    // Extract some useful information from the request
+    let who = event
+        .query_string_parameters_ref()
+        .and_then(|params| params.first("name"))
+        .unwrap_or("world");
+    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+
+    let keyset = KeySet::new("ap-south-1", "ap-south-1_uU90KbaQr")?;
+    let verifier = keyset.new_id_token_verifier(&["78dd765hhdjrutde2vf7g1v73d"]).build()?;
+    // Get the token from the request header Authorization and verify it
+    let token_str = event.headers().get("Authorization").unwrap().to_str().unwrap();
+    keyset.verify(&token_str, &verifier).await?;
+
+    // Return something that implements IntoResponse.
+    // It will be serialized to the right response event automatically by the runtime
+    let resp = Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .body(message.into())
+        .map_err(Box::new)?;
+    Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    run(service_fn(function_handler)).await
+}
