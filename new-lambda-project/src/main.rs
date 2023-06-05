87a06167b1a930f009819d6069739903112bc4fc
Sumeet Naik <25149022+sumeetweb@users.noreply.github.com>
2023-06-05 05:34:04 +0000

    remove custom err Box

--- new-lambda-project/src/main.rs
+++ new-lambda-project/src/main.rs
@@ -2,12 +2,11 @@
 use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 use jsonwebtokens_cognito::KeySet;
 
-pub type LambdaError = Box<dyn std::error::Error + Send + Sync + 'static>;
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, LambdaError> {
+async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     // Extract some useful information from the request
     let who = event
         .query_string_parameters_ref()
